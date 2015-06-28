3f75631a92717ceb17679d747eb17b74d5baa08f
leoMagis <laeliquis@gmail.com>
2015-06-27 19:48:10 -0600

    Setup Fusion Station inventory

--- src/main/java/com/github/leomagis/magis/block/BlockInfusionStation.java
+++ src/main/java/com/github/leomagis/magis/block/BlockInfusionStation.java
@@ -1,8 +1,14 @@
 package com.github.leomagis.magis.block;
 
 import com.github.leomagis.magis.Magis;
+import com.github.leomagis.magis.entity.tile.TileFusionStation;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
 
 public class BlockInfusionStation extends Block {
     public BlockInfusionStation() {
@@ -14,5 +20,54 @@ public class BlockInfusionStation extends Block {
         setHardness(5.0F);
         setResistance(30.0F);
         setStepSound(soundTypeMetal);
+    }
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumFacing side, float hitX, float hitY, float hitZ) {
+        TileFusionStation fusionStation = (TileFusionStation) worldIn.getTileEntity(pos);
+        if (side == EnumFacing.UP) {
+            if ((hitX >= 0) && (hitX <= 0.33)) {
+                if ((hitZ >= 0) && (hitZ <= 0.33)) {
+                    int index = 0;
+                    fusionStation.setSocketContents(index, playerIn);
+                    return true;
+                } else if ((hitZ > 0.33) && (hitZ <= 0.66)) {
+                    int index = 1;
+                    fusionStation.setSocketContents(index, playerIn);
+                    return true;
+                } else if ((hitZ > 0.66) && (hitZ <= 1)) {
+                    int index = 2;
+                    fusionStation.setSocketContents(index, playerIn);
+                    return true;
+                }
+            } else if ((hitX > 0.33) && (hitX <= 0.66)) {
+                if ((hitZ >= 0) && (hitZ <= 0.33)) {
+                    int index = 3;
+                    fusionStation.setSocketContents(index, playerIn);
+                    return true;
+                } else if ((hitZ > 0.33) && (hitZ <= 0.66)) {
+                    int index = 4;
+                    fusionStation.setSocketContents(index, playerIn);
+                    return true;
+                } else if ((hitZ > 0.66) && (hitZ <= 1)) {
+                    int index = 5;
+                    fusionStation.setSocketContents(index, playerIn);
+                    return true;
+                }
+            } else if ((hitX > 0.66) && (hitX <= 1)) {
+                if ((hitZ >= 0) && (hitZ <= 0.33)) {
+                    int index = 6;
+                    fusionStation.setSocketContents(index, playerIn);
+                    return true;
+                } else if ((hitZ > 0.33) && (hitZ <= 0.66)) {
+                    int index = 7;
+                    fusionStation.setSocketContents(index, playerIn);
+                    return true;
+                } else if ((hitZ > 0.66) && (hitZ <= 1)) {
+                    int index = 8;
+                    fusionStation.setSocketContents(index, playerIn);
+                    return true;
+                }
+            }
+        } return false;
     }
 }
--- src/main/java/com/github/leomagis/magis/entity/tile/TileFusionStation.java
+++ src/main/java/com/github/leomagis/magis/entity/tile/TileFusionStation.java
@@ -1,7 +1,138 @@
 package com.github.leomagis.magis.entity.tile;
 
-/**
- * Created by Josh on 6/26/2015.
- */
-public class TileFusionStation {
+import com.github.leomagis.magis.Magis;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IChatComponent;
+
+public class TileFusionStation extends TileEntity implements IInventory {
+
+    public TileFusionStation() {
+        super();
+    }
+
+    private ItemStack[] inventoryContents;
+
+    public ItemStack setSocketContents(int index, EntityPlayer playerIn) {
+        if (index != 4) {
+            ItemStack heldItem = playerIn.getHeldItem();
+            if (this.isItemValidForSlot(index, heldItem)) {
+                if (this.getStackInSlot(index) == null) {
+                    this.setInventorySlotContents(index, heldItem);
+                    return null;
+                } else {
+                    getStackInSlotOnClosing(index);
+                    this.setInventorySlotContents(index, heldItem);
+                }
+            }
+
+        } else {
+            getStackInSlotOnClosing(index);
+        } return null;
+    }
+
+    @Override
+    public int getSizeInventory() {
+        return 9;
+    }
+
+    @Override
+    public ItemStack getStackInSlot(int index) {
+        if (index >= 0 && index < this.inventoryContents.length) {
+            return this.inventoryContents[index];
+        } else { return null;}
+    }
+
+    @Override
+    public ItemStack decrStackSize(int index, int count) {
+        return null;
+    }
+
+    @Override
+    public ItemStack getStackInSlotOnClosing(int index) {
+        if (this.inventoryContents[index] != null)
+        {
+            ItemStack itemstack = this.inventoryContents[index];
+            this.inventoryContents[index] = null;
+            return itemstack;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    @Override
+    public void setInventorySlotContents(int index, ItemStack stack) {
+        this.inventoryContents[index] = stack;
+
+        if (stack != null && stack.stackSize > this.getInventoryStackLimit())
+        {
+            stack.stackSize = this.getInventoryStackLimit();
+        }
+
+        this.markDirty();
+    }
+
+    @Override
+    public int getInventoryStackLimit() {
+        return 1;
+    }
+
+    @Override
+    public boolean isUseableByPlayer(EntityPlayer playerIn) {
+        return false;
+    }
+
+    @Override
+    public void openInventory(EntityPlayer playerIn) {
+
+    }
+
+    @Override
+    public void closeInventory(EntityPlayer playerIn) {
+
+    }
+
+    @Override
+    public boolean isItemValidForSlot(int index, ItemStack stack) {
+        return stack.getItem() == Magis.elementalCompound;
+    }
+
+    @Override
+    public int getField(int id) {
+        return 0;
+    }
+
+    @Override
+    public void setField(int id, int value) {
+
+    }
+
+    @Override
+    public int getFieldCount() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public boolean hasCustomName() {
+        return false;
+    }
+
+    @Override
+    public IChatComponent getDisplayName() {
+        return null;
+    }
 }
