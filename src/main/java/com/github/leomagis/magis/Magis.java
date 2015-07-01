8f42ae1192a76ed364775c4346bec2598f7ecf08
leoMagis <laeliquis@gmail.com>
2015-07-01 00:16:16 -0600

    Mobs have a chance of dropping compounds

--- src/main/java/com/github/leomagis/magis/Magis.java
+++ src/main/java/com/github/leomagis/magis/Magis.java
@@ -10,12 +10,21 @@ import com.github.leomagis.magis.proxy.CommonProxy;
 import com.github.leomagis.magis.recipe.FusionRecipeRegistry;
 import net.minecraft.client.resources.model.ModelBakery;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.boss.EntityWither;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.monster.*;
+import net.minecraft.entity.passive.EntityBat;
+import net.minecraft.entity.passive.EntitySquid;
+import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -191,6 +200,7 @@ public class Magis {
 				stackNegative,
 				stackChaotic,
 				stackDeath
+		//TODO add runic recipes
 		);
 	}
 
@@ -199,5 +209,81 @@ public class Magis {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         proxy.registerRenderers();
+		MinecraftForge.EVENT_BUS.register(this);
     }
-}
+	@SubscribeEvent
+	public void onLivingDropsEvent(LivingDropsEvent event) {
+		EntityItem item = new EntityItem(event.entity.getEntityWorld(), event.entity.posX, event.entity.posY, event.entity.posZ);
+		ItemStack stack = new ItemStack(elementalCompound);
+		Double chance = 0.0;
+
+		if (event.entity instanceof EntityCreeper) {
+			stack.setItemDamage(EnumCompoundType.KINETIC.ordinal());
+			chance = 0.05;
+
+		} else if (event.entity instanceof EntitySkeleton) {
+			if(((EntitySkeleton)event.entity).getSkeletonType() != 1) {
+				stack.setItemDamage(EnumCompoundType.DEATH.ordinal());
+				chance = 0.05;
+			} else {stack.setItemDamage(EnumCompoundType.CHAOTIC.ordinal());
+					chance = 0.5;
+			}
+
+		} else if (event.entity instanceof EntityMagmaCube) {
+			stack.setItemDamage(EnumCompoundType.HEAT.ordinal());
+			chance = 0.11;
+
+		} else if (event.entity instanceof EntitySlime) {
+			stack.setItemDamage(EnumCompoundType.FLUIDIC.ordinal());
+			chance = 0.11;
+
+		} else if (event.entity instanceof EntityZombie) {
+			stack.setItemDamage(EnumCompoundType.DEATH.ordinal());
+			chance = 0.05;
+
+		} else if (event.entity instanceof EntityGhast) {
+			stack.setItemDamage(EnumCompoundType.AIRY.ordinal());
+			chance = 0.5;
+
+		} else if (event.entity instanceof EntityBlaze) {
+			stack.setItemDamage(EnumCompoundType.PLASMA.ordinal());
+			chance = 0.1;
+
+		} else if (event.entity instanceof EntityBat) {
+			stack.setItemDamage(EnumCompoundType.AIRY.ordinal());
+			chance = 0.03;
+
+		} else if (event.entity instanceof EntityWitch) {
+			stack.setItemDamage(EnumCompoundType.PROXIMITY.ordinal());
+			chance = 0.2;
+
+		} else if (event.entity instanceof EntityEndermite) {
+			stack.setItemDamage(EnumCompoundType.NEGATIVE.ordinal());
+			chance = 0.6;
+
+		} else if (event.entity instanceof EntityGuardian) {
+			stack.setItemDamage(EnumCompoundType.LIGHT.ordinal());
+			chance = 0.1;
+
+		} else if (event.entity instanceof EntitySquid) {
+			stack.setItemDamage(EnumCompoundType.WATERY.ordinal());
+			chance = 0.05;
+
+		} else if (event.entity instanceof EntityVillager) {
+			stack.setItemDamage(EnumCompoundType.ORDER.ordinal());
+			chance = 0.3;
+
+		} else if (event.entity instanceof EntityWither) {
+			stack.setItemDamage(EnumCompoundType.KINETIC.ordinal());
+			chance = 0.7;
+
+		} else if (event.entity instanceof EntityIronGolem) {
+			stack.setItemDamage(EnumCompoundType.METALLIC.ordinal());
+			chance = 0.05;
+
+		}
+		if (Math.random() >= chance) {return;}
+			item.setEntityItemStack(stack);
+			event.drops.add(item);
+		}
+	}
